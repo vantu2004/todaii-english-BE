4a5b0cec2992b2ebfee20636fb3668ff83fe8536
Thang Huynh <thanghuynh0706@gmail.com>
2026-05-17 13:33:24 +0700

    feat(toeic-pasage): add modelmapper for image and audio urls

--- infra/src/main/java/com/todaii/english/infra/config/ModelMapperConfig.java
+++ infra/src/main/java/com/todaii/english/infra/config/ModelMapperConfig.java
@@ -5,9 +5,11 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.todaii.english.core.entity.toeic.ToeicPassage;
 import com.todaii.english.core.entity.toeic.ToeicQuestion;
 import com.todaii.english.shared.dto.ToeicQuestionDTO;
 import com.todaii.english.shared.request.server.toeic.Part12Request;
+import com.todaii.english.shared.request.server.toeic.ToeicPassageRequest;
 
 @Configuration
 public class ModelMapperConfig {
@@ -25,6 +27,14 @@ public class ModelMapperConfig {
             m -> {
               m.map(src -> src.getImageRequest().getImageUrl(), ToeicQuestion::setImageUrl);
               m.map(src -> src.getAudioRequest().getAudioUrl(), ToeicQuestion::setAudioUrl);
+            });
+
+    mapper
+        .typeMap(ToeicPassageRequest.class, ToeicPassage.class)
+        .addMappings(
+            m -> {
+              m.map(src -> src.getImageRequest().getImageUrl(), ToeicPassage::setImageUrl);
+              m.map(src -> src.getAudioRequest().getAudioUrl(), ToeicPassage::setAudioUrl);
             });
     mapper
         .typeMap(ToeicQuestion.class, ToeicQuestionDTO.class)
